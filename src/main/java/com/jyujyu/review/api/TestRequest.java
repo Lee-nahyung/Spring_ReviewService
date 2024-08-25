ccad210caf5a5072e1fc331a1e4472f4563f8313
Lee-nahyung <skgud0125@naver.com>
2024-08-25 22:24:37 +0900

    Path Varaiable 방식

--- src/main/java/com/jyujyu/review/api/TestRequest.java
+++ src/main/java/com/jyujyu/review/api/TestRequest.java
@@ -1,6 +1,7 @@
 package com.jyujyu.review.api;
 
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -14,5 +15,14 @@ public class TestRequest {
             @RequestParam("age") Integer age
     ){
         return "Hello, Request Param, I am " + name + ", " + age;
+    }
+
+    //Path Varaiable 방식
+    @GetMapping("/test/path/{name}/{age}")
+    public String requestPathVariable(
+            @PathVariable("name") String name,
+            @PathVariable("age") Integer age
+    ){
+        return "Hello, Path Variable, I am " + name + ", " + age;
     }
 }
