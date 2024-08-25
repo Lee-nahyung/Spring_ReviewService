de03bdec1587df3dfa53cdc6ef8fc408180862c6
Lee-nahyung <skgud0125@naver.com>
2024-08-25 22:46:55 +0900

    Request Body 방식

--- src/main/java/com/jyujyu/review/api/TestRequestAPI.java
+++ src/main/java/com/jyujyu/review/api/TestRequestAPI.java
@@ -1,12 +1,9 @@
 package com.jyujyu.review.api;
 
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
-public class TestRequest {
+public class TestRequestAPI {
 
     //Request parameter 방식
     @GetMapping("test/param")
@@ -25,4 +22,24 @@ public class TestRequest {
     ){
         return "Hello, Path Variable, I am " + name + ", " + age;
     }
+
+    //Request Body 방식
+    @PostMapping("/test/body")
+    public String requestBody(
+            @RequestBody TestRequestBody request
+    ){
+        return "Hello, request body, I am " + request.name + ", " + request.age;
+    }
+
+    public static class TestRequestBody{
+        String name;
+
+        Integer age;
+
+        public TestRequestBody(String name, Integer age) {
+            this.name = name;
+            this.age = age;
+        }
+    }
+
 }
