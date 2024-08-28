ecbd61f1a638b21aea0c6c66ff13c0e6d2d59672
Lee-nahyung <skgud0125@naver.com>
2024-08-29 00:20:26 +0900

    QueryDSL을 활용한 예제, API 작성하기

--- src/main/java/com/jyujyu/review/api/TestQueryApi.java
+++ src/main/java/com/jyujyu/review/api/TestQueryApi.java
@@ -17,4 +17,18 @@ public class TestQueryApi {
     public List<TestEntity> queryJPA(){
         return testService.findAllByNameByJPA("jyujyu");
     }
+    /*여러가지 이름을 가지고 보여줄 수 있는 방식
+    @GetMapping("/test/query/jpa")
+    public List<TestEntity> queryJPA(
+        @RequestParam("name") String name
+        ){
+        return testService.findAllByNameByJPA(name);
+        }
+
+     */
+
+    @GetMapping("/test/query/querydsl")
+    public List<TestEntity> queryQuerydsl(){
+        return testService.findAllByNameByQuerydsl("jyujyu");
+    }
 }
