5ab3751cb051aef7471899d405dfe6cc6f676a5e
Lee-nahyung <skgud0125@naver.com>
2024-09-25 11:21:19 +0900

    맛집 관리 API 스펙 추가

--- src/main/java/com/jyujyu/review/api/RestaurantApi.java
+++ src/main/java/com/jyujyu/review/api/RestaurantApi.java
@@ -1,5 +1,6 @@
 package com.jyujyu.review.api;
 
+import com.jyujyu.review.api.request.CreateAndEditRestaurantRequest;
 import org.springframework.web.bind.annotation.*;
 
 @RestController
@@ -21,24 +22,28 @@ public class RestaurantApi {
 
     //맛집 생성 API
     @PostMapping("/restaurant")
-    public String createRestaurant(){
-        return "This is createRestaurant";
+    public String createRestaurant(
+            @RequestBody CreateAndEditRestaurantRequest request
+            ){
+        return "This is createRestaurant, name= " +request.getName() + "address= " + request.getAddress()
+                + ", menu[0].name= " + request.getMenus().get(0).getName() + ", menu[0].price = " + request.getMenus().get(0).getPrice();
     }
 
-    //맛집 수정 API
     @PutMapping("/restaurant/{restaurantId}")
     public String editRestaurant(
-            @PathVariable Long restaurantId
+            @PathVariable Long restaurantId,
+            @RequestBody CreateAndEditRestaurantRequest request
     ){
-        return "This is editRestaurant" + restaurantId;
+        return "This is editRestaurant ," + restaurantId + " name= " + request.getName() + ", address= " + request.getAddress();
     }
+
 
     //맛집 삭제 API
     @DeleteMapping("/restaurant/{restaurantId}")
     public String deleteRestaurant(
             @PathVariable Long restaurantId
     ){
-        return "This is deleteRestaurant" + restaurantId;
+        return "This is deleteRestaurant ," + restaurantId;
     }
 
 
